6fff4168ab292861fe90e94b198918482182df2b
David Thanphilom <david.thanphilom@teacollection.com>
2019-12-02 15:09:57 -0800

    edited cover letter

--- html2pdf.js
+++ html2pdf.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
-var html = fs.readFileSync('./html/output/resume.html', 'utf8');
+var html = fs.readFileSync('./html/output/cover-letter.html', 'utf8');
 var options = {
   format: 'Letter'
 };
 
-pdf.create(html, options).toFile('./pdf/output/David-Thanphilom-FED-resume.pdf', function(err, res) {
+pdf.create(html, options).toFile('./pdf/output/David-Thanphilom-cover-letter.pdf', function(err, res) {
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
