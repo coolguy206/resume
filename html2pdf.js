08a2569dafac9ad219d8fb4230118dde55c96f08
David Thanphilom <davidthanphilom@msn.com>
2024-04-09 17:40:28 -0700

    added video editing resume

--- html2pdf.js
+++ html2pdf.js
@@ -1,17 +1,24 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
-var html1 = fs.readFileSync('./html/output/resume.html', 'utf8');
+var html1 = fs.readFileSync('./html/output/video-resume.html', 'utf8');
 var html2 = fs.readFileSync('./html/output/cover-letter.html', 'utf8');
 var options = {
   format: 'Letter'
 };
-
+/*
 pdf.create(html2, options).toFile('./pdf/output/David-Thanphilom-cover-letter.pdf', function(err, res) {
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
 
+
 pdf.create(html1, options).toFile('./pdf/output/David-Thanphilom-FED-resume.pdf', function(err, res) {
+  if (err) return console.log(err);
+  console.log(res); // { filename: '/app/businesscard.pdf' }
+});
+*/
+
+pdf.create(html1, options).toFile('./pdf/output/David-Thanphilom-video-resume.pdf', function(err, res) {
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
